e0d4121dac08010c6e71482fe7f56b3913df898b
carsanvel <carvsk@gmail.com>
2021-04-06 09:27:35 +0100

    Dobble, ya se puede tirar la carta al centro (falta mejorar el rango del centro)

--- src/dobble/GameDisplay.java
+++ src/dobble/GameDisplay.java
@@ -22,20 +22,22 @@ public class GameDisplay extends JPanel {
     private Stack<Carta> monton;
     private ManoCartas[] jugadores;
     private Carta carta;
-    private int cartaX;
-    private int cartaY;
+    private int cartaX = (int)(getWidth()/3.2);
+    private int cartaY = getHeight()/16 * 11;
+    boolean pulsado = false;
 
     public GameDisplay(Carta prieraCarta, ManoCartas[] jugadores) {
         monton = new Stack<Carta>();
         monton.push(prieraCarta);
         carta = prieraCarta;
         this.jugadores = jugadores;
-        display(carta);
         CompleteMouseListener mouseListener  = new CompleteMouseListener();
         addMouseListener(mouseListener);
         addMouseMotionListener(mouseListener);
         cartaX = (int)(getWidth()/3.2);
         cartaY = getHeight()/16 * 11;
+        display(carta);
+        repaint();
     }
     
     public void display (Carta carta) {
@@ -61,14 +63,36 @@ public class GameDisplay extends JPanel {
         return false;
     }
     
+    public boolean isInCenter(int x, int y) {
+        int length = (int)(getWidth()/2.5);
+        if(x > (int)(getWidth()/4) + length || x < (int)(getWidth()/4)) {
+            return false;
+        }
+        int centerY = (int) (getHeight()/3.5) + (getHeight()/5);
+        int centerX = getWidth()/4 + (getWidth()/5);
+        int radius = (int) (getHeight() / 5);
+        int xDistance =  Math.abs(x - centerX);
+        int distance = (int) Math.sqrt(Math.pow(radius, 2) - Math.pow(xDistance, 2));
+        int yLimit1 = (int) (centerY + distance);
+        int yLimit2 = (int) (centerY - distance);
+        if (y < yLimit1 && y > yLimit2) {
+            return true;
+        }
+        return false;
+    }
+    
     @Override
     public void paint(Graphics g) {
         g.clearRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
-        g.drawImage(imageOf(carta.getDirectorio()), getWidth()/4, (int) (getHeight()/3.5), (int)(getWidth()/2.5), (int)(getHeight()/2.5), null);
-        g.drawImage(imageOf("carta3"), (int)(getWidth()/3.2), 0, (int) (getWidth()/3.5), (int)(getHeight()/3.5), null);
-        g.drawImage(imageOf("carta2"), cartaX, cartaY, (int) (getWidth()/3.5), (int)(getHeight()/3.5), null);
+        g.drawImage(imageOf(monton.peek().getDirectorio()), getWidth()/4, (int) (getHeight()/3.5), (int)(getWidth()/2.5), (int)(getHeight()/2.5), null);
+        g.drawImage(imageOf(jugadores[1].getPrimera().getDirectorio()), (int)(getWidth()/3.2), 0, (int) (getWidth()/3.5), (int)(getHeight()/3.5), null);
+        if (pulsado) {
+            g.drawImage(imageOf(jugadores[0].getPrimera().getDirectorio()), cartaX, cartaY, (int) (getWidth()/3.5), (int)(getHeight()/3.5), null);
+        } else {
+            g.drawImage(imageOf(jugadores[0].getPrimera().getDirectorio()), (int)(getWidth()/3.2), getHeight()/16 * 11, (int) (getWidth()/3.5), (int)(getHeight()/3.5), null);
+        }
         Graphics2D g2 = (Graphics2D)g;
         g.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(3));
@@ -101,17 +125,25 @@ public class GameDisplay extends JPanel {
         public void mousePressed(MouseEvent e) {
             if (isInRange(e.getX(), e.getY())) {
                 pressed = true;
+                pulsado = true;
                 mouseXPosition = e.getX();
                 mouseYPosition = e.getY();
+                cartaX = (int)(getWidth()/3.2);
+                cartaY = getHeight()/16 * 11;
             }
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
+            if(isInCenter(e.getX(), e.getY())) {
+                monton.push(jugadores[0].getPrimera());
+                jugadores[0].tirarCarta();
+            }
             pressed = false;
             cartaX = (int)(getWidth()/3.2);
             cartaY = getHeight()/16 * 11;
             repaint();
+            pulsado = false;
         }
 
         @Override
